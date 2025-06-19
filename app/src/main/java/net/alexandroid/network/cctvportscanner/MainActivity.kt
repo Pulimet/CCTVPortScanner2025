0623c545688418ccd61b25ec0a80360149f4777c
Korolev, Alexey (ak3140) <ak3140@att.com>
2025-06-19 15:28:56 +0300

    Add TopAppBar

--- app/src/main/java/net/alexandroid/network/cctvportscanner/MainActivity.kt
+++ app/src/main/java/net/alexandroid/network/cctvportscanner/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import net.alexandroid.network.cctvportscanner.ui.home.AppBar
 import net.alexandroid.network.cctvportscanner.ui.home.HomeScreen
 import net.alexandroid.network.cctvportscanner.ui.theme.MyTheme
 
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = { AppBar() }) { innerPadding ->
                     HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
---
+++ app/src/main/java/net/alexandroid/network/cctvportscanner/ui/home/TopAppBar.kt
@@ -0,0 +1,45 @@
+package net.alexandroid.network.cctvportscanner.ui.home
+
+import android.content.res.Configuration
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import net.alexandroid.network.cctvportscanner.R
+import net.alexandroid.network.cctvportscanner.ui.theme.MyTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(stringResource(R.string.app_name))
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppBarPreview() {
+    MyTheme {
+        AppBar()
+    }
+}
+
+@Preview(
+    name = "Dark Mode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+fun AppBarDarkPreview() {
+    MyTheme {
+        AppBar()
+    }
+}
