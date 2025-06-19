d6cd8c0c0cb0b63d3d9f55079b541fbaea6accaf
Korolev, Alexey (ak3140) <ak3140@att.com>
2025-06-19 14:16:02 +0300

    build.gradle.kts - Update version to 1.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.alexandroid.network.cctvportscanner"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 20
+        versionName = "1.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
