727ab8ea133d2ef786575d439646ab638ce7c232
Pulimet <korolev2009@gmail.com>
2025-07-01 10:37:37 +0300

    Add SplashScreen

--- app/src/main/java/net/alexandroid/network/cctvportscanner/MainActivity.kt
+++ app/src/main/java/net/alexandroid/network/cctvportscanner/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import net.alexandroid.network.cctvportscanner.ui.AppWrapper
 import net.alexandroid.network.cctvportscanner.ui.home.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         homeViewModel.onCreate()
         enableEdgeToEdge()
         setContent { AppWrapper() }
