d7e6bef5e91c7e597780f89ebdcdc73dcfd81fd2
Pulimet <korolev2009@gmail.com>
2025-07-06 10:01:51 +0300

    Bottom bar - Add Delete All button

--- app/src/main/java/net/alexandroid/network/cctvportscanner/ui/bottom/BottomBar.kt
+++ app/src/main/java/net/alexandroid/network/cctvportscanner/ui/bottom/BottomBar.kt
@@ -1,6 +1,12 @@
 package net.alexandroid.network.cctvportscanner.ui.bottom
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -10,10 +16,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.alexandroid.network.cctvportscanner.R
 import net.alexandroid.network.cctvportscanner.ui.common.PreviewWrapper
 import net.alexandroid.network.cctvportscanner.ui.dialog.button.ButtonDialogViewModel
@@ -25,8 +35,17 @@ import org.koin.androidx.compose.koinViewModel
 fun BottomBar(buttonDialogViewModel: ButtonDialogViewModel = koinViewModel()) {
     BottomAppBar(
         actions = {
-            IconButton(onClick = { buttonDialogViewModel.deleteAllButtons() }) {
-                Icon(Icons.Filled.Delete, contentDescription = "Delete All")
+            Row(modifier = Modifier.padding(horizontal = 8.dp)) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .clickable { buttonDialogViewModel.deleteAllButtons() }
+                ) {
+                    Icon(Icons.Filled.Delete, contentDescription = stringResource(R.string.delete_all))
+                    Text(text = stringResource(R.string.delete_all).uppercase(), fontSize = 12.sp) // Your visible label
+                }
             }
         },
         floatingActionButton = {
